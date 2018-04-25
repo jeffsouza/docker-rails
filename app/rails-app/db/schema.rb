251cebff7a384ede1912d3c86af856c4586cf9c1
Jefferson <jeffsouza@gmail.com>
2018-04-24 23:02:43 -0300

    Extracted the code to prepare the application to start and added to an image called jeffdesouza/rails-prepare

--- app/rails-app/db/schema.rb
+++ app/rails-app/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180424155855) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
